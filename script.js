381aeefa5a5bd57f82656309bda08c166d57e8fe
Sofia Marhaug <sofiamarhaug@gmail.com>
2021-09-15 12:41:51 +0200

    no errors

--- script.js
+++ script.js
@@ -3,24 +3,62 @@ function log (logmessage) {
     console.log(logmessage);
 }
 
-// declare some constants
-const namn="test"
-const password="1234"
+// declare some constant variables
+const namnConstant="test";
+const lösenordConstant="1234";
 
-// create a function that executes when user clicks button
-//const btn = button.onclick()
+// declare some constants to fetch user input
+const inputName = document.getElementById("inputName");
+const inputPwd = document.getElementById("inputPwd");
+const loginButton = document.getElementById("loginButton");
+
 
 // create a function that checks the input
+function checkInput(namn, lösenord) {
+if (namn === namnConstant && lösenord === lösenordConstant) {
 
-function checkInput() {
-//plocka in input och checka mot consts
-if (namn === "test") {
     log("funkar");
-    document.getElementById("successfulLoginPage").textContent = "Välkommen!"
+    // toggle visibility
+    document.getElementById("successfulLoginBlock").style.display = "block"
+
+    // change content of block
+    document.getElementById("successfulLoginBlockContent").textContent = "Välkommen!"
 } else {
     log("funkar inte")
-    document.getElementById("unsuccessfulLoginPage").textContent = "Nu gick något snett, klicka på knappen och försök att logga in igen."
-    //<input type="button" value="Ok"> <!--gå tillbaka till login page. ta bort!--></input>
+    //toggle visibility
+    document.getElementById("unsuccessfulLoginBlock").style.display = "block"
+
+    //change content of block
+    document.getElementById("unsuccessfulLoginBlockContent").textContent = "Fel namn eller lösenord. Klicka på Ok och försök igen."
 }
 }
-checkInput()
+
+function toggleLoginBlock(){
+    document.getElementById("loginBlock").style.display = "none"
+    log("login-rutan har blivit togglad")
+}
+
+function logOut(params) {
+    //clear local storage
+    localStorage.clear()
+
+
+}
+
+// create a function that executes when user clicks button
+loginButton.onclick = function() {
+    const namn = inputName.value;
+    const lösenord = inputPwd.value;
+
+    localStorage.setItem("name", namn);
+    localStorage.setItem("password", lösenord);
+
+    //check if input exists and is correct
+    if (namn && lösenord) {
+        checkInput(namn, lösenord);
+        //show in log
+        console.log(localStorage);
+    } else {
+        console.log("Input cannot be empty!")
+    }
+}
