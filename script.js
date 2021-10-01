f3fd3a25e5bb250f24946e32a3b7df22c28c71ae
Sofia Marhaug <sofiamarhaug@gmail.com>
2021-10-01 13:02:45 +0200

    Rewrote everything to use insertAdjecentHtml instead of document.createElement

--- script.js
+++ script.js
@@ -3,127 +3,89 @@
 const namn = "test";
 const lösenord = "1234";
 
-// declare some variables for user input
-const inputName = document.getElementById("inputName");
-const inputPwd = document.getElementById("inputPwd");
+// declare some constants
+const loginPage = document.getElementById("loginPage");
 
-//declare some variables for user action
-const loginButton = document.getElementById("loginButton");
-
-
-// create a function that checks the user input
-function checkInput(inputName, inputPwd) {
-    if (inputName === namn && inputPwd === lösenord) {
-
-        //hide visibility of main block
-        changeVisibility("loginBlock", "hide");
-        //create and show the welcome block
-        createSuccessfulLoginBlock();
-
-    } else {
-        //hide visibility of main block
-        changeVisibility("loginBlock", "hide");
-
-        //create and show the unsuccessful login block
-        createUnsuccessfulLoginBlock();
-    }
-}
-
-//add functionality to login button
-loginButton.onclick = function () {
-    const namn = inputName.value;
-    const lösenord = inputPwd.value;
-
-    localStorage.setItem("name", namn);
-
-    //check if input exists and is correct
-    if (namn && lösenord) {
-        checkInput(namn, lösenord);
-    } else {
-        alert("Namn och/eller lösenord får ej vara tomt. Försök igen.");
-    }
-
-    //clear input fields
-    inputName.value = "";
-    inputPwd.value = "";
+//check if user is already logged in
+if (localStorage.getItem("name") !== null) {
+    console.log(localStorage);
+    createSuccessfulLoginPage();
+} else {
+    createLoginPage();
 }
 
 
-//create a function that builds the welcome page
+//create the login page
+function createLoginPage() {
 
-function createSuccessfulLoginBlock() {
+    //clear page first
+    loginPage.innerHTML = "";
 
-    //create the paragraph for the welcome text
-    const welcomeParagraph = document.createElement("p");
-    welcomeParagraph.innerText = "Du är inloggad. Välkommen!";
+    loginPage.insertAdjacentHTML('afterbegin', '<label for="namn">Namn:</label><br>');
+    loginPage.insertAdjacentHTML('beforeend', '<input type="text" id="inputName" placeholder="Skriv namn" autocomplete="off"></input>');
+    loginPage.insertAdjacentHTML('beforeend', '<br><label for="lösenord">Lösenord:</label><br>');
+    loginPage.insertAdjacentHTML('beforeend', '<input type="password" id="inputPwd" placeholder="Skriv lösenord" autocomplete="off"</input>');
+    loginPage.insertAdjacentHTML('beforeend', '<button id="loginButton">Logga in</button>');
+
+    loginButton.onclick = function () {
+
+        inputName = document.getElementById("inputName").value;
+        inputPwd = document.getElementById("inputPwd").value;
+    
+        //check if input exists and is correct
+        if (inputName && inputPwd) {
+            checkInput(inputName, inputPwd);
+        } else {
+            alert("Namn och/eller lösenord får ej vara tomt. Försök igen.");
+        }
+    }
+}
 
 
-    //create the log out button
-    const logoutButton = document.createElement("button");
-    logoutButton.innerText = "Logga ut";
+//create the welcome page
+function createSuccessfulLoginPage() {
 
-    //create a div to enclose it all
-    const successfulLoginBlock = document.createElement("div");
-    successfulLoginBlock.id = "successfulLoginBlock";
-    successfulLoginBlock.appendChild(welcomeParagraph);
-    successfulLoginBlock.appendChild(logoutButton);
+    //clear page
+    loginPage.innerHTML = "";
 
-    //add the new div element to the doc body
-    document.body.appendChild(successfulLoginBlock);
+    loginPage.insertAdjacentHTML('beforeend', '<p>Du är inloggad. Välkommen!');
+    loginPage.insertAdjacentHTML('beforeend', '<button id="logoutButton">Logga ut</button>');
 
     // add functionality to log out button
     logoutButton.onclick = function () {
         logOut();
     }
-
 }
 
-function createUnsuccessfulLoginBlock() {
+//create the unsuccessful login page
+function createUnsuccessfulLoginPage() {
+    loginPage.innerHTML = "";
 
-    //create the paragraph for the warning text
-    const UnsuccessfulLoginParagraph = document.createElement("p");
-    UnsuccessfulLoginParagraph.innerText = "Fel namn eller lösenord. Klicka på Ok och försök igen.";
-
-    //create and add functionality to ok button
-    const okButton = document.createElement("button");
-    okButton.innerText = "Ok";
-
-    //create a div to enclose it all
-    const unsuccessfulLoginBlock = document.createElement("div");
-    unsuccessfulLoginBlock.id = "unsuccessfulLoginBlock";
-    unsuccessfulLoginBlock.appendChild(UnsuccessfulLoginParagraph);
-    unsuccessfulLoginBlock.appendChild(okButton);
-
-    //add the div to the doc body
-    document.body.appendChild(unsuccessfulLoginBlock);
+    loginPage.insertAdjacentHTML('beforeend', '<p>Fel namn eller lösenord. Klicka på Ok och försök igen.</p>');
+    loginPage.insertAdjacentHTML('beforeend', '<button id="okButton">Ok</button>');
 
     //add functionality to ok button
     okButton.onclick = function () {
-        changeVisibility("loginBlock", "show");
-        changeVisibility("unsuccessfulLoginBlock", "hide");
-
+        createLoginPage();
     }
-
 }
 
+// create a function that checks the user input
+function checkInput(inputName, inputPwd) {
 
-//create a function that toggles block visibility
-function changeVisibility(blockName, visibility) {
-    if (visibility === "show") {
-        visibility = "block";
+
+    if (inputName == namn && inputPwd == lösenord) {
+        localStorage.setItem("name", namn);
+        createSuccessfulLoginPage();
     } else {
-        visibility = "none";
+        createUnsuccessfulLoginPage();
     }
-    //toggle the block visibility
-    document.getElementById(blockName).style.display = visibility;
 }
 
 //create a function for when user logs out
 function logOut() {
-    //go back to login block
-    changeVisibility("loginBlock", "show");
-    changeVisibility("successfulLoginBlock", "hide");
 
     //clear local storage
     localStorage.removeItem("name");
+    createLoginPage();
 }
